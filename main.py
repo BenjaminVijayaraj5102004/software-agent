541e53b2b1b7c42889840729d94bac3be4b0b676
Benjamin V <benjaminvijayaraj5102004@gmail.com>
2026-06-22 09:19:34 +0530

    Update project files

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routers.authentication import router as authentication
 from app.routers.token import router as Token
-
+from app.routers.conversation import router as conversation
+from app.routers.message import router as Messages
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -22,4 +23,6 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(authentication)
 app.include_router(Token)
+app.include_router(conversation)
+app.include_router(Messages)
 
