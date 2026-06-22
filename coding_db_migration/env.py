dc616a57da0b0a0de904719d60d5cff70b280dd5
Benjamin V <benjaminvijayaraj5102004@gmail.com>
2026-06-22 11:52:57 +0530

    alembic intilization removed debug statements

--- coding_db_migration/env.py
+++ coding_db_migration/env.py
@@ -29,7 +29,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-print(Base.metadata.tables.keys())
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
